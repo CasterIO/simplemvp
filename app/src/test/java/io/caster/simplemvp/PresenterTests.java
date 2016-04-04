3246bc87e6f63522ae48f444a056435c02eed00f
Donn Felker <donn@donnfelker.com>
2016-04-03 21:30:09 -0400

    updating tests

--- app/src/test/java/io/caster/simplemvp/PresenterTests.java
+++ app/src/test/java/io/caster/simplemvp/PresenterTests.java
@@ -9,6 +9,9 @@ import io.caster.simplemvp.presentation.UserPresenterImpl;
 import io.caster.simplemvp.repository.UserRepository;
 import io.caster.simplemvp.view.UserView;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -86,5 +89,87 @@ public class PresenterTests {
         verify(mockView, never()).displayLastName(anyString());
     }
 
+    @Test
+    public void shouldShouldErrorMessageDuringSaveIfFirstOrLastNameIsMissing() {
+        when(mockView.getUserId()).thenReturn(1);
 
+        // Load the user
+        presenter.loadUserDetails();
+
+        verify(mockView, times(1)).getUserId();
+
+        // Set up the view mock
+        when(mockView.getFirstName()).thenReturn(""); // empty string
+
+        presenter.saveUser();
+
+        verify(mockView, times(1)).getFirstName();
+        verify(mockView, never()).getLastName();
+        verify(mockView, times(1)).showUserNameIsRequired();
+
+        // Now tell mockView to return a value for first name and an empty last name
+        when(mockView.getFirstName()).thenReturn("Foo");
+        when(mockView.getLastName()).thenReturn("");
+
+        presenter.saveUser();
+
+        verify(mockView, times(2)).getFirstName(); // Called two times now, once before, and once now
+        verify(mockView, times(1)).getLastName();  // Only called once
+        verify(mockView, times(2)).showUserNameIsRequired(); // Called two times now, once before and once now
+    }
+
+    @Test
+    public void shouldBeAbleToSaveAValidUser() {
+        when(mockView.getUserId()).thenReturn(1);
+
+        // Load the user
+        presenter.loadUserDetails();
+
+        verify(mockView, times(1)).getUserId();
+
+        when(mockView.getFirstName()).thenReturn("Foo");
+        when(mockView.getLastName()).thenReturn("Bar");
+
+        presenter.saveUser();
+
+        // Called two more times in the saveUser call.
+        verify(mockView, times(2)).getFirstName();
+        verify(mockView, times(2)).getLastName();
+
+        assertThat(user.getFirstName(), is("Foo"));
+        assertThat(user.getLastName(), is("Bar"));
+
+        // Make sure the repository saved the user
+        verify(mockUserRepository, times(1)).save(user);
+
+        // Make sure that the view showed the user saved message
+        verify(mockView, times(1)).showUserSavedMessage();
+    }
+
+
+    @Test
+    public void shouldNotHaveInteractionsOnPause() {
+        presenter.pause();
+
+        verifyZeroInteractions(mockUserRepository);
+        verifyZeroInteractions(mockView);
+    }
+
+    @Test
+    public void shouldLoadUserDetailsWhenResumeCalled() {
+        presenter.resume();
+
+        verify(mockUserRepository, times(1)).getUser(anyInt());
+        verify(mockView, times(1)).displayFirstName(anyString());
+        verify(mockView, times(1)).displayLastName(anyString());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionWhenViewIsNull() {
+        // Null out the view
+        presenter.setView(null);
+
+        // Try to load the screen which will force interactions with the view
+        presenter.loadUserDetails();
+    }
 }
